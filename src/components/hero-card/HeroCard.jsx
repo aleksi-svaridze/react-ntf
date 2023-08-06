5fc846dc3d10edf60efe631e1a6477c6a145967c
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-06 13:41:40 +0400

    Finish: Hero Section

--- src/components/hero-card/HeroCard.jsx
+++ src/components/hero-card/HeroCard.jsx
@@ -6,7 +6,7 @@ const HeroCard = () => {
     return (
         <div className='hero-card'>
             <img src={heroImg} alt="" className='w-100' />
-            <div className='bg-black-secondary'>
+            <div className='bg-black-secondary custom-p-20'>
                 <h5 className='text-capitalize fw-semibold mb-2'>Space Walking</h5>
                 <User />
             </div>
