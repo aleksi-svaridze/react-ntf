f8060a160bf669645695b6f124963bd6dc77bba6
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-13 09:03:00 +0400

    Start: Categories section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./layout/navbar/Navbar";
 import Hero from "./layout/hero/Hero";
 import Collection from "./layout/collection/Collection";
 import Creators from "./layout/creators/Creators";
+import Categories from "./layout/categories/Categories";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Collection />
       <Creators />
+      <Categories />
     </div>
   );
 }
