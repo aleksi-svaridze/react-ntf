927e544ece393b29cf25586cde7a1fa19dd161ca
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-16 13:44:24 +0400

    Add images to Categories cards

--- src/components/categories-card/CategoriesCard.jsx
+++ src/components/categories-card/CategoriesCard.jsx
@@ -3,14 +3,14 @@
 const CategoriesCard = ({id, image, icon, text}) => {
     return (
         <div className="col-6 col-xl-3">
-            <div className="cat-card bg-black-secondary overflow-hidden" key={id}>
-                <div className="w-100 h-100 position-relative">
+            <a href={text} className="cat-card text-decoration-none d-block bg-black-secondary" key={id}>
+                <div className="w-100 h-100 position-relative overflow-hidden">
                     <img src={image} alt="" className='w-100 cat-card__image' />
                     <img src={icon} alt="" className='position-absolute top-50 start-50 translate-middle cat-card__icon' />
                 </div>
             
                 <p className="text-capitalize cat-card__text text-white fw-semibold m-0">{text}</p>
-            </div>
+            </a>
         </div>
     )
 }
--- src/layout/categories/Categories.jsx
+++ src/layout/categories/Categories.jsx
@@ -5,36 +5,43 @@ import artImage from '../../media/images/categories/cat-art.svg';
 import brushIcon from '../../media/images/categories/brush_icon.svg';
 
 import swatchesIcon from '../../media/images/categories/swatches_icon.svg';
+import collectiblesImage from '../../media/images/categories/collectibles.png';
 
 import musicNotesIcon from '../../media/images/categories/MusicNotes.svg';
+import musicImage from '../../media/images/categories/music.svg';
 
 import cameraIcon from '../../media/images/categories/Camera.svg';
+import cameraImage from '../../media/images/categories/photography.svg';
 
 import videoIcon from '../../media/images/categories/VideoCamera.svg';
+import videoImage from '../../media/images/categories/videoImage.svg';
 
 import utilityIcon from '../../media/images/categories/MagicWand.svg';
+import utilityImage from '../../media/images/categories/utilityImage.svg';
 
 import ballIcon from '../../media/images/categories/Basketball.svg';
+import sportImage from '../../media/images/categories/sportImage.svg';
 
 import planetIcon from '../../media/images/categories/Planet.svg';
+import worldsImage from '../../media/images/categories/worlds.svg';
 
 
 const Categories = () => {
     const categories = [
         {id: 1, image: artImage, icon: brushIcon, text: 'art'},
-        {id: 2, image: artImage, icon: swatchesIcon, text: 'Collectibles'},
-        {id: 3, image: artImage, icon: musicNotesIcon, text: 'Music'},
-        {id: 4, image: artImage, icon: cameraIcon, text: 'Photography'},
-        {id: 5, image: artImage, icon: videoIcon, text: 'Video'},
-        {id: 6, image: artImage, icon: utilityIcon, text: 'Utility'},
-        {id: 7, image: artImage, icon: ballIcon, text: 'Sport'},
-        {id: 8, image: artImage, icon: planetIcon, text: 'Virtual Worlds'},
+        {id: 2, image: collectiblesImage, icon: swatchesIcon, text: 'Collectibles'},
+        {id: 3, image: musicImage, icon: musicNotesIcon, text: 'Music'},
+        {id: 4, image: cameraImage, icon: cameraIcon, text: 'Photography'},
+        {id: 5, image: videoImage, icon: videoIcon, text: 'Video'},
+        {id: 6, image: utilityImage, icon: utilityIcon, text: 'Utility'},
+        {id: 7, image: sportImage, icon: ballIcon, text: 'Sport'},
+        {id: 8, image: worldsImage, icon: planetIcon, text: 'Virtual Worlds'},
     ]
     return (
         <div className="container py-4 mt-5">
             <PrimaryHeading heading='Browse Categories'/>
 
-            <div className="row g-4 py-4 my-1 py-lg-5 my-lg-2">
+            <div className="row g-3 g-lg-4 py-4 my-1 py-lg-5 my-lg-2">
                 {categories.map(item => (
                         <CategoriesCard 
                         id={item.id} 
