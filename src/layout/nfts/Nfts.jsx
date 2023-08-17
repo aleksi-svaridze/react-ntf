fe070ae485a8a3fc950ca41081d39d9164619836
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-17 11:43:55 +0400

    Add: Button component to nfts section

--- src/layout/nfts/Nfts.jsx
+++ src/layout/nfts/Nfts.jsx
@@ -1,6 +1,7 @@
 import { PrimaryHeading } from "../../components/headings/Headings";
 import NftsCard from "../../components/nfts-card/NftsCard";
 import eyeIcon from '../../media/images/nfts/Eye.svg';
+import Buttons from '../../components/buttons/Buttons'
 
 const Nfts = () => {
     return (
@@ -15,6 +16,12 @@ const Nfts = () => {
             <div className="row g-4 py-4 my-1 py-lg-5 my-lg-2">
                 <NftsCard />
             </div>
+            
+            <Buttons 
+                icon={eyeIcon} 
+                content={'See All'} 
+                btnStyle='text-white mt-3 btn_custom btn_custom--black heading-btn d-lg-none d-flex' 
+            /> 
         </div>
     )
 }
