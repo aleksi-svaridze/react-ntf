3d4b60a4409c9e6eeae8cd5a20f9459dfc5607c8
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-17 11:03:14 +0400

    Nfts component

--- src/components/nfts-card/NftsCard.jsx
+++ src/components/nfts-card/NftsCard.jsx
@@ -1,34 +1,47 @@
-import galaxyImage from '../../media/images/nfts/galaxy.svg';
+import Sale from '../sales/Sale';
 import User from '../../components/users/User';
-import user1 from '../../media/images/nfts/user-1.svg';
+
+import galaxyImage from '../../media/images/nfts/galaxy.png';
+import user1 from '../../media/images/nfts/user-1.png';
+
+import edenImage from '../../media/images/nfts/edena.png';
+import user2 from '../../media/images/nfts/user-2.png';
+
+import astroImage from '../../media/images/nfts/astro.png';
+import user3 from '../../media/images/nfts/user-3.png';
+
 
 const NftsCard = () => {
+    const nftsCardData = [
+        {id: 1, price: '1.63 ETH', bid: '0.33 wETH', image: galaxyImage, text: 'Distant Galaxy', user: user1, userName: 'MoonDancer'},
+        {id: 2, price: '1.63 ETH', bid: '0.33 wETH', image: edenImage, text: 'Life On Edena', user: user2, userName: 'NebulaKid'},
+        {id: 3, price: '1.63 ETH', bid: '0.33 wETH', image: astroImage, text: 'AstroFiction', user: user3, 
+    userName: 'Spaceone'},
+    ]
     return (
-        <div className="col-12 col-lg-4">
-            <div className="nfts-card bg-black-secondary">
-                <img src={galaxyImage} alt="" className='w-100' />
-                <div className='nfts-card__text-content'>
-                    <h2 className='nfts-card__text-content-title text-white text-capitalize fw-semibold'>Distant Galaxy</h2>
-                    <User 
-                        imageSrc={user1} 
-                        content='MoonDancer'
-                        contentClass='font-spacemono'
-                    />
-
-                    <div className='wrapper'>
-                        <div className=''>
-                            <span className='text-capitalize'>price</span>
-                            <span>1.63 ETH</span>
-                        </div>
-                        <div className=''>
-                            <span className='text-capitalize'>Highest Bid</span>
-                            <span>0.33 wETH</span>
+      <>
+            {
+                nftsCardData.map(item => (
+                    <div className="col-12 col-lg-4">
+                        <div className="nfts-card">
+                            <img src={item.image} alt={item.text} className='w-100' />
+                            <div className='nfts-card__text-content bg-black-secondary'>
+                                <h2 className='nfts-card__text-content-title text-white text-capitalize fw-semibold'>{item.text}</h2>
+                                <User 
+                                    imageSrc={item.user} 
+                                    content={item.userName}
+                                    contentClass='font-spacemono'
+                                />
+                                <Sale 
+                                    price={item.price} 
+                                    bid={item.bid}
+                                />
+                            </div>
                         </div>
                     </div>
-
-                </div>
-            </div>
-        </div>
+                ))
+            }
+        </>
     )
 }
 
---
+++ src/components/sales/Sale.jsx
@@ -0,0 +1,18 @@
+
+
+const Sale = ({price, bid}) => {
+    return (
+        <div className='sale d-flex align-items-center justify-content-between'>
+            <div>
+                <p className='text-capitalize mb-2 sale__desc font-spacemono text-black-tertiary'>price</p>
+                <p className='mb-0 sale__amount text-white font-spacemono'>{price}</p>
+            </div>
+            <div>
+                <p className='text-capitalize mb-2 sale__desc font-spacemono text-black-tertiary'>Highest Bid</p>
+                <p className='mb-0 sale__amount text-white font-spacemono'>{bid}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Sale;
