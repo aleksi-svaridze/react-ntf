313dd8a6bb3e420584f114e5147560c72f7e12cd
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-08-17 11:57:03 +0400

    Add text-end class to sale-s component

--- src/components/sales/Sale.jsx
+++ src/components/sales/Sale.jsx
@@ -3,11 +3,11 @@
 const Sale = ({price, bid}) => {
     return (
         <div className='sale d-flex align-items-center justify-content-between'>
-            <div>
+            <div className="text-start">
                 <p className='text-capitalize mb-2 sale__desc font-spacemono text-black-tertiary'>price</p>
                 <p className='mb-0 sale__amount text-white font-spacemono'>{price}</p>
             </div>
-            <div>
+            <div className="text-end">
                 <p className='text-capitalize mb-2 sale__desc font-spacemono text-black-tertiary'>Highest Bid</p>
                 <p className='mb-0 sale__amount text-white font-spacemono'>{bid}</p>
             </div>
