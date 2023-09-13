9162c5116de60b7b01478575d10db872fd2f20c7
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-09-14 00:08:51 +0400

    Add useHook package && cover component to top-creators single page

--- src/pages/TopCreator.jsx
+++ src/pages/TopCreator.jsx
@@ -1,9 +1,21 @@
+import { useWindowSize } from "usehooks-ts";
+
+import userCoverDesktop from '../media/images/top-creators/cover/desktop/cover.jpg'
+import userCoverMobile from '../media/images/top-creators/cover/mobile/cover.jpg'
+import userProfileImg from '../media/images/top-creators/user-image/user-avatar.png'
 
 
 const TopCreator = () => {
+    let {width} = useWindowSize();
     return(
-        <div>
-            hello, super duper creator!
+        <div 
+            className="top-creators__profile" 
+            style={{backgroundImage: `url(${width >= 992 ? userCoverDesktop : userCoverMobile})`}}>
+                <div className="container h-100">
+                    <div className="layout-container position-relative h-100">
+                        <img src={userProfileImg} alt="" className="position-absolute top-creators__profile--img" />
+                    </div>
+                </div>
         </div>
     )
 }
