55ddfe150aad7aeea7110b1c4c966e2d188934ee
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-10-13 14:53:36 +0400

    Add Tag component to NFTs page

---
+++ src/components/tag/Tag.jsx
@@ -0,0 +1,9 @@
+
+
+const Tag = ({content}) => {
+    return(
+        <div className="tag tag--content text-white fw-semibold text-uppercase d-flex align-items-center justify-content-center bg-black-secondary">{content}</div>
+    )
+}
+
+export default Tag;
--- src/pages/NftsPage.jsx
+++ src/pages/NftsPage.jsx
@@ -10,6 +10,8 @@ import Timer from '../components/timer/Timer'
 import data from '../data/creators/creators.json'
 import NftsCard from '../components/nfts-card/NftsCard'
 import Button from '../components/buttons/Buttons'
+import User from '../components/users/User'
+import Tag from '../components/tag/Tag'
 
 const NftsPage= () => {
     const {width} = useWindowSize();
@@ -17,7 +19,6 @@ const NftsPage= () => {
     let result = null;
 
     for(let i = 0; i < data.length; i++) {
-        console.log(data[i])
         if(data[i].id === '7') {
             result = data[i];
         }
@@ -33,7 +34,7 @@ const NftsPage= () => {
             <div className="layout-container py-4">
                 <div className="container py-3">
                     <div className="row">
-                        <div className="col-12 col-md-6 col-lg-8 bg-danger">
+                        <div className="col-12 col-md-6 col-lg-8">
                             <SecondaryHeading customStyles='fourthiary-heading' content='The Orbitians' />
                             <Desc content='Minted on Sep 30, 2022' customStyles={'text-black-tertiary'} />
 
@@ -44,12 +45,39 @@ const NftsPage= () => {
                                 btnStyle={'btn_custom btn_custom--purple btn_custom--purple-width mx-auto d-flex font-worksans mt-4 text-capitalize'} />
                             </div>
 
-                            <h3 className={`fourthiary-desc font-spacemono text-black-tertiary mb-2 ${width >= 768 ? 'fw-semibold' : 'fw-normal'}`}>Created By</h3>
-                            <p className='fourthiary-desc mb-0'>The Orbitians</p>
-                            <p className='fourthiary-desc'>is a collection of 10,000 unique NFTs on the Ethereum blockchain,</p>
-                            <p className='fourthiary-desc'>There are all sorts of beings in the NFT Universe. The most advanced and friendly of the bunch are Orbitians.</p>
-                            <p className='fourthiary-desc'>They live in a metal space machines, high up in the sky and only have one foot on Earth.
-These Orbitians are a peaceful race, but they have been at war with a group of invaders for many generations. The invaders are called Upside-Downs, because of their inverted bodies that live on the ground, yet do not know any other way to be. Upside-Downs believe that they will be able to win this war if they could only get an eye into Orbitian territory, so they've taken to make human beings their target.</p>
+                            <div className='bg-danger'>
+                                <h3 className={`fourthiary-desc font-spacemono text-black-tertiary mb-2 ${width >= 768 ? 'fw-semibold' : 'fw-normal'}`}>Created By</h3>
+                                <User 
+                                    userContainer=''
+                                    imageSrc={result.userImage} 
+                                    content={result.name} 
+                                    contentClass=''
+                                />
+                            </div>
+
+                            <div className='bg-danger'>
+                                <h3 className={`fourthiary-desc font-spacemono text-black-tertiary mb-2 ${width >= 768 ? 'fw-semibold' : 'fw-normal'}`}>Description</h3>
+                                <p className='fourthiary-desc mb-0'>The Orbitians</p>
+                                <p className='fourthiary-desc'>is a collection of 10,000 unique NFTs on the Ethereum blockchain,</p>
+                                <p className='fourthiary-desc'>There are all sorts of beings in the NFT Universe. The most advanced and friendly of the bunch are Orbitians.</p>
+                                <p className='fourthiary-desc'>They live in a metal space machines, high up in the sky and only have one foot on Earth. These Orbitians are a peaceful race, but they have been at war with a group of invaders for many generations. The invaders are called Upside-Downs, because of their inverted bodies that live on the ground, yet do not know any other way to be. Upside-Downs believe that they will be able to win this war if they could only get an eye into Orbitian territory, so they've taken to make human beings their target.</p>
+                            </div>
+
+                            <div className='bg-danger'>
+                                <h3 className={`fourthiary-desc font-spacemono text-black-tertiary mb-2 ${width >= 768 ? 'fw-semibold' : 'fw-normal'}`}>Details</h3>
+                            </div>
+
+                            <div className='bg-danger'>
+                                <h3 className={`fourthiary-desc font-spacemono text-black-tertiary mb-2 ${width >= 768 ? 'fw-semibold' : 'fw-normal'}`}>Tags</h3>
+                                <div className='d-flex flex-column flex-lg-row align-items-lg-center align-items-start gap-3'>
+                                    {
+                                        result.tags.map(tag => (
+                                            <Tag content={tag} />
+                                        ))
+                                    }
+                                </div>
+                            </div>
+
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
                             <div className='d-md-flex justify-content-end align-items-start d-none'>
